6588c84cd06e9f0eaf1537cc922298770f710d0e
Alfredo Daniel Sanchez <45202555+alfredos84@users.noreply.github.com>
2023-01-17 12:28:21 +0100

    Update cuOPO.sh

--- src/cuOPO.sh
+++ src/cuOPO.sh
@@ -48,13 +48,15 @@ ARG2=14                              # Grid size = 2^(ARG2)                     
 ARG3=150                             # Number of crystal partitions                               (ARG3)
 ARG4=(5)                             # Lcav = ARG5*Lcr (cavity length in terms of crystal length) (ARG4)
 RE=(98)                              # Reflectivity at signal wl (in percent %)                   (ARG5)
-D=(0)                                # Net cavity detuning (in pi rad)                            (ARG6)
+D=(0)                                # Net cavity detuning (in rad)                               (ARG6)
 GD=(0)                               # GDD compensation (in percent %)                            (ARG7)
 ARG8=10000                           # Number of round trips per simulation                       (ARG8)
-NN=(3.5)                             # N = Power/Pth                                              (ARG9)
-UPM=(0)                              # Using phase modulator: OFF/ON = 0/1                        (ARG10)
-MD=(0)                               # EOM: β (modulation depth in π rads)                        (ARG11)
-FM=(0)                               # δf = FSR - fpm [MHz] Frequency detuning for EOM            (ARG12)
+NN=(4)                               # N = Power/Pth                                              (ARG9)
+UPM=(1)                              # Using phase modulator: OFF/ON = 0/1                        (ARG10)
+MD=(0.8)                             # EOM: β (modulation depth in π rads)                        (ARG11)
+FM=(2)                               # δf = FSR - fpm [MHz] Frequency detuning for EOM            (ARG12)
+TD=(0)                               # TOD compensation (in percent %)                            (ARG13)
+SPM=(0)                              # Using self-phase modulation: OFF/ON = 0/1                  (ARG14)
 
 # Each for-loop span over one or more values defined in the previous arguments. 
 for (( u=0; u<${#UPM[@]}; u++ ))
@@ -70,41 +72,46 @@ do
 					for (( r=0; r<${#RE[@]}; r++ ))
 					do	
 						for (( g=0; g<${#GD[@]}; g++ ))
-						do	
-							U=${UPM[$u]} 
-							printf "\nUsing phase mod  = ${UPM}\n" 
-							
-							N=${NN[$n]} 
-							printf "\nPower/Pth        = ${N} \n" 
-							
-							GDD=${GD[$g]} 
-							printf "\nGDD compensation = ${GDD}%%\n"
-							
-							R=${RE[$r]} 
-							printf "\nLcav/Lc          = ${ARG4} \n"
-							
-							DELTAS=${D[$i]}
-							printf "\ndelta            = ${DELTAS}\n"
-							
-							MODDEP=${MD[$m]}
-							printf "\nModul depth      = ${MODDEP}\n"
-							
-							FREQMOD=${FM[$f]}
-							printf "\nFreqMod          = ${FREQMOD} MHz\n"
-							
-							printf "\nMaking directory...\n"
-							FOLDER="${REG}_${EQS}eqs_PPLN_delta_${DELTAS}_POWER_${N}"
-							FILE="${REG}_${EQS}eqs_PPLN_delta_${DELTAS}_POWER_${N}.txt"
-							
-							mkdir $FOLDER		
-							
-							printf "Bash execution and writing output file...\n\n"
-							
-							./cuOPO $ARG1 $ARG2 $ARG3 $ARG4 $R $DELTAS $GDD $ARG8 $N $U $MODDEP $FREQMOD | tee -a $FILE
+						do
+							for (( t=0; t<${#TD[@]}; t++ ))
+							do
+								U=${UPM[$u]} 
+								printf "\nUsing phase mod  = ${UPM}\n" 
+								
+								N=${NN[$n]} 
+								printf "\nPower/Pth        = ${N} \n" 
+								
+								GDD=${GD[$g]} 
+								printf "\nGDD compensation = ${GDD}%%\n"
+								
+								TOD=${TD[$t]}
+								printf "\nTOD compensation = ${TOD}%%\n"
+								
+								R=${RE[$r]} 
+								printf "\nLcav/Lc          = ${ARG4} \n"
+								
+								DELTAS=${D[$i]}
+								printf "\ndelta            = ${DELTAS}\n"
+								
+								MODDEP=${MD[$m]}
+								printf "\nModul depth      = ${MODDEP}\n"
+								
+								FREQMOD=${FM[$f]}
+								printf "\nFreqMod          = ${FREQMOD} \n"
 
-							printf "Bash finished!!\n\n" 
-							mv *.dat $FOLDER"/"
-							mv *.txt $FOLDER"/"
+								
+								printf "\nMaking directory...\n"
+								FOLDER="${REG}_${EQS}eqs_PPLN_beta_${MODDEP}_N_${N}_GDD_${GDD}"
+								FILE="${REG}_${EQS}eqs_PPLN_beta_${MODDEP}_N_${N}_GDD_${GDD}.txt"
+								
+								printf "Bash execution and writing output file...\n\n"
+								./$EXEC_FILE $ARG1 $ARG2 $ARG3 $ARG4 $R $DELTAS $GDD $ARG8 $N $U $MODDEP $FREQMOD $TOD $SPM | tee -a $FILE
+						# 			
+								printf "Bash finished!!\n\n" 
+								mkdir $FOLDER
+								mv *.dat $FOLDER"/"
+								mv *.txt $FOLDER"/"
+							done
 						done
 					done
 				done
